b74003ffcea3cc2a310e6e56c8a104aa9ce2f7c7
Francisco Morales <fmorales9172@gmail.com>
2026-05-20 11:44:47 -0400

    feat: expose minimal preload bridge (version + connectivity)

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,2 +1,10 @@
-// Minimal preload; expanded in Task 5.
-export {};
+import { contextBridge, ipcRenderer } from 'electron';
+
+type ConnectivityStatus = 'online' | 'offline';
+
+contextBridge.exposeInMainWorld('sombrasolDesktop', {
+  getVersion: (): Promise<string> => ipcRenderer.invoke('app:get-version'),
+  onConnectivityChange: (callback: (status: ConnectivityStatus) => void): void => {
+    ipcRenderer.on('connectivity', (_event, status: ConnectivityStatus) => callback(status));
+  },
+});
